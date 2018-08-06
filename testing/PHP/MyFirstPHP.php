b2a2b8c9feb04f2253abc108656926ff77cc7609
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2018-08-06 11:03:33 +0200

    First Upload

---
+++ testing/PHP/MyFirstPHP.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php 
+	$Hello = "Hello World";
+	$ThisIs = "This is PHP";
+	//Pass is pennis
+	echo $Hello . " " . $ThisIs;
+ ?>
+
+</body>
+</html>
